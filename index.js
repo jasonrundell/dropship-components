7070a4fe9eb0663d4b9361716e5b22ef2de68781
jasonrundell <jason.rundell@gmail.com>
2018-06-15 14:44:20 -0400

    Enabling Image again

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import Card from './lib/components/Card';
 import Heading from './lib/components/Heading';
-//import Image from './lib/components/Image';
+import Image from './lib/components/Image';
 import Layout from './lib/components/Layout';
 import Paragraph from './lib/components/Paragraph';
 
 export {
   Card,
   Heading,
+  Image,
   Layout,
   Paragraph
 };
