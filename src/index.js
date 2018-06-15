6cf504173e60c48b81368daac992d61d429963e2
jasonrundell <jason.rundell@gmail.com>
2018-06-15 12:48:49 -0400

    Restructure

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import Card from './Components/Card';
+import Heading from './Components/Heading';
+import Image from './Components/Image';
+import Layout from './Components/Layout';
+import Paragraph from './Components/Paragraph';
+
+export {
+  Card,   
+  Heading,
+  Image,
+  Layout,
+  Paragraph
+};
