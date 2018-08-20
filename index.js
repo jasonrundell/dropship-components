ba3cfbd58b958ebb874c8088e0445c3a00aea7ab
codeblink <codeblinklive@gmail.com>
2018-08-19 23:50:14 -0400

    Address added

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+import Address from './lib/components/Address';
+import Article from './lib/components/Article';
 import Button from './lib/components/Button';
 import Card from './lib/components/Card';
 import Form from './lib/components/Form';
+import Hgroup from './lib/components/Hgroup';
 import Heading from './lib/components/Heading';
 import Image from './lib/components/Image';
 import InputSubmit from './lib/components/InputSubmit';
@@ -10,9 +13,11 @@ import Layout from './lib/components/Layout';
 import Link from './lib/components/Link';
 import Paragraph from './lib/components/Paragraph';
 import OrderedList from './lib/components/OrderedList';
+import Section from './lib/components/Section';
 import UnorderedList from './lib/components/UnorderedList';
 
 export {
+  Address,
   Article,
   Button,
   Card,
